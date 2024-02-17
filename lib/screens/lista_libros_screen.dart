f535ebd7c6ccc7b2eae4203d2baaf21a348351b0
MiguelC007 <103707813+MiguelC007@users.noreply.github.com>
2024-02-16 20:21:37 -0600

    4

---
+++ lib/screens/lista_libros_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:primerexamen1_miguelc/core/libros_core.dart';
+import 'package:provider/provider.dart';
+
+import 'detalle_libros_screeen.dart';
+
+class BookListScreen extends StatefulWidget {
+  @override
+  _BookListScreenState createState() => _BookListScreenState();
+}
+
+class _BookListScreenState extends State<BookListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() =>
+        Provider.of<BookListProvider>(context, listen: false).fetchBook());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Libros'),
+      ),
+      body: Consumer<BookListProvider>(
+        builder: (context, bookListProvider, child) {
+          if (bookListProvider.books.isEmpty) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+            itemCount: bookListProvider.books.length,
+            itemBuilder: (context, index) {
+              final book = bookListProvider.books[index];
+              return ListTile(
+                title: Text(book.title),
+                subtitle: Text(book.description),
+                leading: Image.network(
+                  'https://mewmagazine.es/wp-content/uploads/2020/06/mientras-escribo-1-768x1165.jpg',
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => bookDetailScreen(book: book),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
